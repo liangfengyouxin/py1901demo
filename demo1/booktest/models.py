09af9dfff91cda6ada2fc9cb7a102f439e703de0
zzt <875692416@qq.com>
2019-04-16 15:59:19 +0800

    后台管理+数据库操作

--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.btitle
 
 
 class HeroInfo(models.Model):
@@ -12,3 +14,5 @@ class HeroInfo(models.Model):
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=20)
     hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)
+    def __str__(self):
+        return self.hname
--- demo1/demo1/settings.py
+++ demo1/demo1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'booktest',
+    'booktest',     #进行应用的注册
 ]
 
 MIDDLEWARE = [
