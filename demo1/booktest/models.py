b4b6ecf4042baf5fb031fdded158fe948c660c86
zzt <875692416@qq.com>
2019-04-16 11:52:04 +0800

    连接SQLite数据库

--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField(auto_now_add=True)
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=20)
+    hbook = models.ForeignKey('BookInfo',on_delete=models.CASCADE)
